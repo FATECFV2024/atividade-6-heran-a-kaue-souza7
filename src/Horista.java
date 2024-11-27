7962467b439db60fbf1ed76cb5b37714357c37de
Kauê <kauekaue960@gmail.com>
2024-11-27 10:12:04 -0300

    [feat] incluindo a classe "Horista"

---
+++ src/Horista.java
@@ -0,0 +1,23 @@
+public class Horista extends Empregado {
+    private double horas;
+
+    public Horista(String n, String e, double h) {
+        super(n, e);
+        horas = h;
+    }
+
+    public double getHoras() {
+        return horas;
+    }
+
+    public void setHoras(double h) {
+        horas = h;
+
+    }
+
+    
+    public void calcularSalario() {
+        salario = horas * 50;
+    }
+    
+}
