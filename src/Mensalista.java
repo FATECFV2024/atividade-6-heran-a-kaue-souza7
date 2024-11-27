96d768e3cca87008a98fb96d63675c0acff8b6de
Kauê <kauekaue960@gmail.com>
2024-11-27 10:07:41 -0300

    [feat] Inclusão da classe "Mensalista"

---
+++ src/Mensalista.java
@@ -0,0 +1,27 @@
+public class Mensalista extends Empregado {
+    private String cargo;
+
+    public Mensalista(String n, String e, String c) {
+        super(n, e);
+        cargo = c;
+    }
+
+    public String getCargo() {
+        return (cargo);
+    }
+
+    public void setCargo(String c) {
+        cargo = c;
+    }
+
+    public void calcularSalario() {
+        if (cargo.equals("Junior")) {
+            salario = 2500.00;
+        } else if (cargo.equals("Pleno")) {
+            salario = 3500.00;
+        } else {
+            salario = 5500.00;
+        }
+    }
+    
+}
